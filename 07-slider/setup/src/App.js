06a224d22b727e001622ffc86bf8e3242d5f4a71
Minji <321963741@naver.com>
2021-08-26 18:31:39 +0900

    8-lorem-page project done

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
+import React, {useState, useEffect} from 'react';
+import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
+import {FaQuoteRight} from 'react-icons/fa';
 import data from './data';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [person, setPerson] = useState(data);
+  const [page, setPage] = useState(0);
+  return (
+    <>
+      <section className="section">
+        <div class="container">
+          <h1 className="title">review</h1>
+        </div>
+        <div class="section-center">
+          {person.map((each) => {
+            const {id, image, name, title, quote} = each;
+            return <img class="person-img" src={image} alt={name} />;
+          })}
+        </div>
+      </section>
+      )
+    </>
+  );
 }
 
 export default App;
--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [text, setText] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let numberPage = Number(page);
+    if (numberPage <= 0) {
+      numberPage = 1;
+    }
+    if (numberPage > data.length) {
+      numberPage = data.length + 1;
+    }
+    setText(data.slice(0, numberPage));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>lorem ipsum project setup</h3>
+      <form className="lerem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs :</label>
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          value={page}
+          onChange={(e) => setPage(e.target.value)}
+        ></input>
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lerem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
