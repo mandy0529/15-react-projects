c251fce56ea298709815473296ec29b487d752d9
Minji <321963741@naver.com>
2021-08-26 21:33:00 +0900

    09-color-gradient half done

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,51 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, {useState} from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+    setColor('');
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#F23432"
+            value={!color ? `#${color}` : color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section class="colors">
+        {list.map((color, index) => (
+          <SingleColor key={index} {...color} index={index} hex={color.hex} />
+        ))}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, {useState, useEffect} from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({weight, rgb, index, hex}) => {
+  const bcg = rgb.join(',');
 
-export default SingleColor
+  const changeColor = () => {
+    const color = `${index > 12 ? '#fff' : ''}`;
+    return color;
+  };
+
+  return (
+    <>
+      <article className="color" style={{backgroundColor: `rgb(${bcg})`}}>
+        <p style={{color: changeColor()}} className="percent-value">
+          {' '}
+          {weight}%
+        </p>
+        <p style={{color: changeColor()}} className="color-value">
+          #{hex}
+        </p>
+      </article>
+    </>
+  );
+};
+
+export default SingleColor;
