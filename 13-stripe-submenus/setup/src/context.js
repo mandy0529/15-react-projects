036ceecfe747c08f1130895374b2c7f3ca5722cd
Minji <321963741@naver.com>
2021-08-30 20:33:01 +0900

    for tomorrow! stripe ing

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,43 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, {useState, useContext} from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+  const [sidebar, setSidebar] = useState(true);
+  const [submenu, setSubmenu] = useState(false);
+
+  const openSidebar = () => {
+    setSidebar(true);
+  };
+  const closeSidebar = () => {
+    setSidebar(false);
+  };
+  const openSubmenu = () => {
+    setSubmenu(true);
+  };
+  const closeSubmenu = () => {
+    setSubmenu(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        sidebar,
+        submenu,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalState = () => {
+  return useContext(AppContext);
+};
+
+export default AppProvider;
