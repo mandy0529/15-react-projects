2f7360df3d4fa0f3433e057b15a1e2b972d17012
Minji <321963741@naver.com>
2021-08-30 13:21:29 +0900

    11 navbar project done

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,51 +1,60 @@
 import React, {useState, useRef, useEffect} from 'react';
-import {FaBars, FaTwitter, FaYoutube, FaFacebook} from 'react-icons/fa';
+import {FaBars} from 'react-icons/fa';
 import {links, social} from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const handleClick = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showNavbar) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showNavbar]);
+
   return (
     <nav>
       <div class="nav-center">
         <div class="nav-header">
           <img src={logo} alt="logo"></img>
-          <button className="nav-toggle">
+          <button onClick={handleClick} className="nav-toggle">
             <FaBars />
           </button>
         </div>
-        <div class="links-container show-container">
-          <ul class="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">twitter</a>
-            </li>
-            <li>
-              <a href="#">github</a>
-            </li>
-            <li>
-              <a href="#">instagram</a>
-            </li>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul class="links" ref={linksRef}>
+            {links.map((link) => {
+              const {id, url, text} = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
-        <ul class="social-icons">
-          <li>
-            <a href="#">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaYoutube />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaFacebook />
-            </a>
-          </li>
-        </ul>
+
+        {
+          <ul class="social-icons">
+            {social.map((item) => {
+              const {id, url, icon} = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
+          </ul>
+        }
       </div>
     </nav>
   );
