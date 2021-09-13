bf5024bb4872c3c884ad517381f2f57fd26ef712
Minji <321963741@naver.com>
2021-09-13 18:00:28 +0900

    23 quiz done

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import {useGlobalContext} from './context';
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+
+import Quiz from './Quiz';
+
 function App() {
-  return <h2>quiz starter</h2>
+  const {waiting, loading, index, questions} = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (waiting) {
+    return <SetupForm {...questions} />;
+  }
+  console.log(questions, '퀘스쳔');
+  return <Quiz {...questions[index]} />;
 }
 
-export default App
+export default App;
--- 23-quiz/setup/src/Modal.js
+++ 23-quiz/setup/src/Modal.js
@@ -1,8 +1,22 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import {useGlobalContext} from './context';
 
 const Modal = () => {
-  return <h2>modal component</h2>
-}
+  const {handleModalClose, questions, score, modal} = useGlobalContext();
+  return (
+    <div className={`${modal ? 'modal-container isOpen' : 'modal-container'}`}>
+      <div className="modal-content">
+        <h2>congrats!</h2>
+        <h2>
+          your score is "{((score / questions.length) * 100).toFixed(0)}"
+          awesome!
+        </h2>
+        <button className="close-btn" onClick={handleModalClose}>
+          play again
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
